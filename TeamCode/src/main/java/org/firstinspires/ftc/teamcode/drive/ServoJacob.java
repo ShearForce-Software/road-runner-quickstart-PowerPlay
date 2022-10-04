ce4a187e80f49adde22d56de1dca4a1b664cd33b
Jacob <jacoblemanski@stdominichs.org>
2022-10-03 21:15:22 -0500

    Jacob Servo Test - move liftOne servo with left_stick_y joystick

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/ServoJacob.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/ServoJacob.java
@@ -27,51 +27,42 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode.drive;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
-/**
- * This OpMode scans a single servo back and forward until Stop is pressed.
- * The code is structured as a LinearOpMode
- * INCREMENT sets how much to increase/decrease the servo position each cycle
- * CYCLE_MS sets the update period.
- *
- * This code assumes a Servo configured with the name "left_hand" as is found on a Robot.
- *
- * NOTE: When any servo position is set, ALL attached servos are activated, so ensure that any other
- * connected servos are able to move freely before running this test.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-@TeleOp(name = "Concept: Scan Servo", group = "Concept")
-@Disabled
-public class  ConceptScanServo extends LinearOpMode {
+
+@TeleOp(name = "Jacob Servo")
+
+public class ServoJacob extends LinearOpMode {
 
     static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
-    static final int    CYCLE_MS    =   50;     // period of each cycle
+ //   static final int    CYCLE_MS    =   50;   // period of each cycle
     static final double MAX_POS     =  1.0;     // Maximum rotational position
     static final double MIN_POS     =  0.0;     // Minimum rotational position
 
+    enum Direction { FORWARD, REVERSE }         // Define direction of servo
+
     // Define class members
-    Servo   servo;
-    double  position = (MAX_POS - MIN_POS) / 2; // Start at halfway position
-    boolean rampUp = true;
+    Servo  liftOne;
+    double  position = 0.0;// (MAX_POS - MIN_POS) / 2; // Start at halfway position
+   // boolean rampUp = true;
 
 
     @Override
     public void runOpMode() {
 
-        // Connect to servo (Assume Robot Left Hand)
+
         // Change the text in quotes to match any servo name on your robot.
-        servo = hardwareMap.get(Servo.class, "left_hand");
+        liftOne = hardwareMap.get(Servo.class, "lift1");
+
+        liftOne.setDirection(Servo.Direction.FORWARD);
 
         // Wait for the start button
-        telemetry.addData(">", "Press Start to scan Servo." );
+        telemetry.addData(">", "Press Start to move lifeOne Servo with left_stick_y joystick." );
         telemetry.update();
         waitForStart();
 
@@ -80,20 +71,20 @@ public class  ConceptScanServo extends LinearOpMode {
         while(opModeIsActive()){
 
             // slew the servo, according to the rampUp (direction) variable.
-            if (rampUp) {
+            if (gamepad1.left_stick_y < 0) {
                 // Keep stepping up until we hit the max value.
                 position += INCREMENT ;
                 if (position >= MAX_POS ) {
                     position = MAX_POS;
-                    rampUp = !rampUp;   // Switch ramp direction
+
                 }
             }
-            else {
-                // Keep stepping down until we hit the min value.
+            else if (gamepad1.left_stick_y > 0){
+                // Keep stepping down until we hit the minimum value.
                 position -= INCREMENT ;
                 if (position <= MIN_POS ) {
                     position = MIN_POS;
-                    rampUp = !rampUp;  // Switch ramp direction
+
                 }
             }
 
@@ -103,8 +94,8 @@ public class  ConceptScanServo extends LinearOpMode {
             telemetry.update();
 
             // Set the servo to the new position and pause;
-            servo.setPosition(position);
-            sleep(CYCLE_MS);
+            liftOne.setPosition(position);
+            //sleep(CYCLE_MS);
             idle();
         }
 
